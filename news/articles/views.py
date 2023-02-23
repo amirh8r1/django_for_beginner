5de5e85d7aa8b433e418376a93cdb42dde77c558
Amir Hossein Rajabi <108143052+amirh8r1@users.noreply.github.com>
2023-02-23 17:21:07 +0330

    add create page

--- news/articles/views.py
+++ news/articles/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import DeleteView, UpdateView
+from django.views.generic.edit import DeleteView, UpdateView, CreateView
 from django.urls import reverse_lazy
 from .models import Article
 
@@ -25,3 +25,12 @@ class ArticleDeleteView(DeleteView):
     model = Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
+
+class ArticleCreateView(CreateView):
+    model = Article
+    fields = (
+        'title',
+        'body',
+        'author',
+    )
+    template_name = 'article_new.html'
