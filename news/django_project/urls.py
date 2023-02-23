25448c51e3fa524e97101d4d57bbfe44270ea07c
Amir Hossein Rajabi <108143052+amirh8r1@users.noreply.github.com>
2023-02-23 15:35:39 +0330

    add articles page

--- news/django_project/urls.py
+++ news/django_project/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('', include('pages.urls'), name='home'), # path('', TemplateView.as_view(template_name='home.html'), name='home')
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('articles/', include('articles.urls')),
 ]
